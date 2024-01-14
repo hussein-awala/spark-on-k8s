e1552ddb7e522568cd9172199bdfc2f94e900ab9
hussein-awala <hussein@awala.fr>
2024-01-14 23:08:38 +0100

    Add docstring to kubernetes_client module

--- spark_on_k8s/kubernetes_client.py
+++ spark_on_k8s/kubernetes_client.py
@@ -6,7 +6,14 @@ from kubernetes import client as k8s, config
 
 
 class KubernetesClientManager:
-    """Kubernetes client manager."""
+    """Kubernetes client manager.
+
+    Args:
+        config_file (str, optional): Path to the Kubernetes configuration file. Defaults to None.
+        context (str, optional): Kubernetes context. Defaults to None.
+        client_configuration (k8s.Configuration, optional): Kubernetes client configuration.
+            Defaults to None.
+    """
 
     def __init__(
         self,
@@ -14,14 +21,25 @@ class KubernetesClientManager:
         context: str | None = None,
         client_configuration: k8s.Configuration | None = None,
     ) -> None:
-        """Create a Kubernetes client manager."""
         self.config_file = config_file
         self.context = context
         self.client_configuration = client_configuration
 
     @contextmanager
     def client(self) -> k8s.ApiClient:
-        """Create a Kubernetes client."""
+        """Create a Kubernetes client in a context manager.
+
+        Examples:
+            >>> from spark_on_k8s.kubernetes_client import KubernetesClientManager
+            >>> with KubernetesClientManager().client() as client:
+            ...     api = k8s.CoreV1Api(client)
+            ...     namespaces = [ns.metadata.name for ns in api.list_namespace().items]
+            ...     print(namespaces)
+            ['default', 'kube-node-lease', 'kube-public', 'kube-system', 'spark']
+
+        Yields:
+            k8s.ApiClient: Kubernetes client.
+        """
         _client = None
         try:
             _client = self.create_client()
@@ -31,7 +49,14 @@ class KubernetesClientManager:
                 _client.close()
 
     def create_client(self) -> k8s.ApiClient:
-        """Get a Kubernetes client."""
+        """Load the Kubernetes configuration and create a Kubernetes client.
+
+        This method could be overridden to create a Kubernetes client in a different way without
+        overriding the client method.
+
+        Returns:
+            k8s.ApiClient: Kubernetes client.
+        """
         config.load_kube_config(
             config_file=self.config_file,
             context=self.context,

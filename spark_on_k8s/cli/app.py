7eba051629e2c1341e669d1b85279a4f9b98ac1b
hussein-awala <hussein@awala.fr>
2024-01-21 19:50:10 +0100

    Implement submit command

--- spark_on_k8s/cli/app.py
+++ spark_on_k8s/cli/app.py
@@ -1,8 +1,24 @@
 from __future__ import annotations
 
+from typing import Literal
+
 import click
 
-from spark_on_k8s.cli.options import app_id_option, force_option, namespace_option
+from spark_on_k8s.cli.options import (
+    app_id_option,
+    app_name_option,
+    app_path_option,
+    class_name_option,
+    docker_image_option,
+    force_option,
+    image_pull_policy_option,
+    logs_option,
+    namespace_option,
+    service_account_option,
+    spark_conf_option,
+    ui_reverse_proxy_option,
+    wait_option,
+)
 
 
 @click.group(name="app")
@@ -60,3 +76,51 @@ def wait(app_id: str, namespace: str):
     app_manager.wait_for_app(namespace=namespace, app_id=app_id)
     app_status = app_manager.app_status(namespace=namespace, app_id=app_id)
     print(f"App {app_id} has terminated with status {app_status.value}")
+
+
+@app_cli.command(
+    params=[
+        docker_image_option,
+        app_path_option,
+        namespace_option,
+        service_account_option,
+        app_name_option,
+        spark_conf_option,
+        class_name_option,
+        wait_option,
+        logs_option,
+        image_pull_policy_option,
+        ui_reverse_proxy_option,
+    ]
+)
+@click.argument("app_arguments", nargs=-1, type=str)
+def submit(
+    image: str,
+    path: str,
+    namespace: str,
+    service_account: str,
+    name: str | None,
+    spark_conf: dict[str, str],
+    class_name: str | None,
+    wait: bool,
+    logs: bool,
+    image_pull_policy: Literal["Always", "Never", "IfNotPresent"],
+    ui_reverse_proxy: bool,
+    app_arguments: tuple[str, ...],
+):
+    from spark_on_k8s.client.generic import SparkOnK8S
+
+    spark_client = SparkOnK8S()
+    spark_client.submit_app(
+        image=image,
+        app_path=path,
+        namespace=namespace,
+        service_account=service_account,
+        app_name=name,
+        spark_conf=spark_conf,
+        class_name=class_name,
+        app_waiter="print" if logs else "wait" if wait else "no_wait",
+        image_pull_policy=image_pull_policy,
+        ui_reverse_proxy=ui_reverse_proxy,
+        app_arguments=list(app_arguments),
+    )
--- spark_on_k8s/cli/options.py
+++ spark_on_k8s/cli/options.py
@@ -9,8 +9,83 @@ namespace_option = click.Option(
     help="The namespace to operate on. Default is `default`.",
 )
 
-
 app_id_option = click.Option(("--app-id",), type=str, required=True, help="The ID of the app to operate on.")
 
-
 force_option = click.Option(("-f", "--force"), is_flag=True, default=False, help="Force the operation.")
+
+
+# Submit options
+def validate_spark_conf(ctx, param, value):
+    spark_conf = {}
+    for conf in value:
+        try:
+            key, val = conf.split("=")
+            spark_conf[key] = val
+        except ValueError:
+            raise click.BadParameter("Spark conf parameter must be in the form key=value.") from None
+
+
+docker_image_option = click.Option(
+    ("--image",),
+    type=str,
+    required=True,
+    help="The docker image to use for the app.",
+)
+app_path_option = click.Option(
+    ("--path",),
+    type=str,
+    required=True,
+    help="The path to the app to submit.",
+)
+service_account_option = click.Option(
+    ("--service-account",),
+    type=str,
+    default="spark",
+    help="The service account to use for the app.",
+)
+app_name_option = click.Option(
+    ("--name",),
+    type=str,
+    required=None,
+    help="The name of the app.",
+)
+spark_conf_option = click.Option(
+    ("--conf", "spark_conf"),
+    type=str,
+    multiple=True,
+    callback=validate_spark_conf,
+    help="Spark configuration property in key=value format. Can be repeated.",
+)
+class_name_option = click.Option(
+    ("--class", "class_name"),
+    type=str,
+    default=None,
+    help="The main class for the app.",
+)
+wait_option = click.Option(
+    ("--wait",),
+    type=bool,
+    is_flag=True,
+    default=False,
+    help="Wait for the app to finish.",
+)
+logs_option = click.Option(
+    ("--logs",),
+    type=bool,
+    is_flag=True,
+    default=False,
+    help="Print the app logs.",
+)
+image_pull_policy_option = click.Option(
+    ("--image-pull-policy",),
+    type=click.Choice(["Always", "IfNotPresent", "Never"]),
+    default="IfNotPresent",
+    help="The image pull policy.",
+)
+ui_reverse_proxy_option = click.Option(
+    ("--ui-reverse-proxy",),
+    type=bool,
+    is_flag=True,
+    default=False,
+    help="Whether to enable UI reverse proxy.",
+)

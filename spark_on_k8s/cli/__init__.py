ac149530be4aa260ed2006ec40ede0845d0873e5
Hussein Awala <hussein@awala.fr>
2024-02-28 21:14:44 +0100

    fix(cli): skip adding api commands when api extra is not installed (#8)

--- spark_on_k8s/cli/__init__.py
+++ spark_on_k8s/cli/__init__.py
@@ -2,10 +2,14 @@ from __future__ import annotations
 
 import click
 
-from spark_on_k8s.cli.api import api_cli
 from spark_on_k8s.cli.app import app_cli
 from spark_on_k8s.cli.apps import apps_cli
 from spark_on_k8s.cli.namespace import namespace_cli
+
+try:
+    from spark_on_k8s.cli.api import api_cli
+except ImportError:
+    api_cli = None
 
 
 @click.group()
@@ -16,8 +20,9 @@ def cli():
 def main():
     cli.add_command(app_cli)
     cli.add_command(apps_cli)
-    cli.add_command(api_cli)
     cli.add_command(namespace_cli)
+    if api_cli:
+        cli.add_command(api_cli)
     cli(max_content_width=120)
 
 

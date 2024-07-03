b17b5643d24f636faadffa2c3a59fa4bce66e790
hussein-awala <hussein@awala.fr>
2024-07-03 23:57:00 +0200

    feat(python-client): support creating and mounting an ephemeral configmap to the driver

--- spark_on_k8s/utils/types.py
+++ spark_on_k8s/utils/types.py
@@ -1,8 +1,35 @@
 from __future__ import annotations
 
+from typing import TypedDict, Union
+
+try:
+    # Python 3.11+
+    from typing import NotRequired, Required
+except ImportError:
+    from typing_extensions import NotRequired, Required
+
 
 class ArgNotSet:
     """A type used to indicate that an argument was not set."""
 
 
 NOTSET = ArgNotSet()
+
+
+class _TextConfigMapSource(TypedDict):
+    name: Required[str]
+    text: Required[str]
+
+
+class _TextFileConfigMapSource(TypedDict):
+    name: Required[str]
+    text_path: Required[str]
+
+
+ConfigMapSource = Union[_TextConfigMapSource, _TextFileConfigMapSource]
+
+
+class ConfigMap(TypedDict):
+    name: NotRequired[str]
+    mount_path: Required[str]
+    sources: Required[list[ConfigMapSource]]

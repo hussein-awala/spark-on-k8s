d1789370d0ebfd4c30b545ef9680574fe9d5c3d5
Hussein Awala <hussein@awala.fr>
2024-12-04 00:53:13 +0100

    feat(airflow): create SparkSqlOnK8SOperator to execute Spark SQL commands (#109)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,18 @@
 from __future__ import annotations
 
+import subprocess
 import sys
 
+import pytest
+
 PYTHON_313_OR_ABOVE = sys.version_info.major == 3 and sys.version_info.minor >= 13
+
+
+@pytest.fixture(scope="session")
+def root_path() -> str:
+    return (
+        subprocess.Popen(["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE)
+        .communicate()[0]
+        .rstrip()
+        .decode("utf-8")
+    )

1f7e282e70ab95fb02b1dcdaf3e708fb41f2cbee
Hussein Awala <hussein@awala.fr>
2024-12-05 01:08:07 +0100

    chore: add sqlfluff to lint spark sql queries (#113)

--- examples/airflow/iceberg_query.sql
+++ examples/airflow/iceberg_query.sql
@@ -3,12 +3,19 @@ CREATE TABLE IF NOT EXISTS prod.db.sample (
     data string,
     category string
 )
-USING iceberg
+USING ICEBERG
 PARTITIONED BY (category);
 
 MERGE INTO prod.db.sample t
-USING (SELECT * FROM prod.db.another_sample WHERE category = 'foo') s
-ON t.id = s.id
+USING (
+    SELECT
+        id,
+        data,
+        category
+    FROM prod.db.another_sample
+    WHERE category = 'foo'
+) s
+    ON t.id = s.id
 WHEN MATCHED THEN UPDATE SET *
 WHEN NOT MATCHED THEN INSERT *
-WHEN NOT MATCHED BY SOURCE THEN DELETE;
+WHEN NOT MATCHED BY SOURCE THEN DELETE; -- noqa: PRS
--- examples/airflow/query.sql
+++ examples/airflow/query.sql
@@ -7,8 +7,8 @@ USING PARQUET
 PARTITIONED BY (student_id INT);
 
 INSERT INTO students VALUES
-    ('Amy Smith', '123 Park Ave, San Jose', {{ ts }}, 111111);
+('Amy Smith', '123 Park Ave, San Jose', '{{ ts }}', 111111);
 
 INSERT INTO students VALUES
-    ('Bob Brown', '456 Taylor St, Cupertino', {{ ts }}, 222222);,
-    ('Cathy Johnson', '789 Race Ave, Palo Alto', {{ ts}}, 333333);
+('Bob Brown', '456 Taylor St, Cupertino', '{{ ts }}', 222222),
+('Cathy Johnson', '789 Race Ave, Palo Alto', '{{ ts }}', 333333);
